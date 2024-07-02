7d52c130f3913d677e2f455576cc5207f18cfc13
neilhatfield <phiguy1114@gmail.com>
2024-07-02 10:47:01 -0400

    update sidebar footer

--- app.R
+++ app.R
@@ -53,7 +53,7 @@ ui <- list(
       ),
       tags$div(
         class = "sidebar-logo",
-        boastUtils::psu_eberly_logo("reversed")
+        boastUtils::sidebarFooter()
       )
     ),
     ### Create the content ----
